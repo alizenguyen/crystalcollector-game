b5ff159ca254ccba436f11a3900fd2ffb91ebf83
Alize Nguyen <alizenguyen@gmail.com>
2017-10-11 22:27:21 -0500

    javascript fixed and finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,112 @@ $(".button").on("click", function() {
 	alert ("You will be given a random number at the start of the game. There are four crystals below. By clicking on a crystal you will add a specific amoutn of points to your score. You win the game by matching your total sore to random number, you lose the game if your total score goes above the random number. The value of each crystal is hidden from you until you click on it. Each time when the game starts, the game will change values of each crystal.");
 });
 
-//Computer chooses random number between 10-120
-
+//Variables 
 var computerChoice = Math.floor(Math.random() * 110) + 10;
+var crystalVal = [Math.floor(Math.random() * 12) + 1];
+var crystalTotal = [];
+var yourScore = 0;
+var wins = 0;
+var losses = 0;
+var crystalOne;
+var crystalTwo;
+var crystalThree;
+var crystalFour;
 
 console.log(computerChoice);
 
-//Computer chooses random number between 10-120
 
-function computerGuess() { 
-	return computerChoice
-};
 
-$("#score-chosen").html(computerChoice);
+$(document).ready(function() { 
 
-//When crystal is clicked - add number between 1-12 to score
+	function startOver() {
 
-//Player wins if total score matches random number. RESET.
+			//Computer chooses random number between 10-120
 
-//Player loses if scor goes above random number. RESET.
+			computerChoice = Math.floor(Math.random() * 110) + 10;
+			computerChoice = parseInt(computerChoice);
 
-//Game reset. Sees new random number. User's score will reset to 0. All 4 crystals will have new hidden values. 
+			$("#score-chosen").html(computerChoice);
+
+			//Adding between 1-12 to score when clicked
+
+			for (var i = 1; i < 5; i++) {
+				crystalVal = [Math.floor(Math.random() * 12) + 1];
+			    console.log("crystalVal = " + crystalVal);
+
+			    // Push random CrystalVal into Crystal Vals array
+			    crystalTotal.push(crystalVal);
+			    console.log("crystalTotal = " + crystalTotal);
+
+			    };
+	
+	}; //startOver function
+
+	//Button functions
+
+	$("#crystalOne").on("click", function() {
+		crystalOne = parseInt(crystalTotal[0]);
+		yourScore += crystalOne;
+		console.log("crystalOne = " + crystalOne);
+	    console.log("yourScore = " + yourScore);
+	    $("#total-score").html(yourScore);
+	    winloss();
+	});
+
+	$("#crystalTwo").on("click", function() {
+		crystalTwo = parseInt(crystalTotal[1]);
+		yourScore += crystalTwo;
+		console.log("crystalTwo = " + crystalTwo);
+	    console.log("yourScore = " + yourScore);
+	    $("#total-score").html(yourScore);
+	    winloss();
+	});
+
+	$("#crystalThree").on("click", function() {
+		crystalThree = parseInt(crystalTotal[2]);
+		yourScore += crystalThree;
+		console.log("crystalThree = " + crystalThree);
+	    console.log("yourScore = " + yourScore);
+	    $("#total-score").html(yourScore);
+	    winloss();
+	});
+
+	$("#crystalFour").on("click", function() {
+		crystalFour = parseInt(crystalTotal[3]);
+		yourScore += crystalFour;
+		console.log("crystalThree = " + crystalFour);
+	    console.log("yourScore = " + yourScore);
+	    $("#total-score").html(yourScore);
+	    winloss();
+	});
+
+	//Game reset. Sees new random number. User's score will reset to 0. All 4 crystals will have new hidden values. 
+
+	function reset() {
+		yourScore = 0;
+		crystalVal = 0;
+		crystalTotal = [];
+	};
+
+	//WINS AND LOSSES
+	function winloss() {
+		if (yourScore === computerChoice) {
+			wins++; 
+			$("#total-wins").html(wins);
+			reset();
+
+			startOver();
+
+		} else if (yourScore > computerChoice) {
+			losses++;
+			$("#total-losses").html(losses);
+			reset();
+
+			startOver();
+		}
+	};
+
+startOver(); 
+
+}); //. Document Ready
+
